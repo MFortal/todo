9eb376cf85df2d27a8edb61349b52fb0ba5dccc2
Polyakova Maria <polyakovamy@yandex.ru>
2022-11-26 16:52:22 +0300

    Добавление файлов при редактировании задачи

--- src/pages/Modal/Edit/Edit.jsx
+++ src/pages/Modal/Edit/Edit.jsx
@@ -6,7 +6,7 @@ import Button from "../../../components/Button/Button";
 import Modal from "../Modal";
 
 import { doc, updateDoc, Timestamp } from "firebase/firestore";
-import { db } from "../../../firebase";
+import { db, storage } from "../../../firebase";
 import {
   FLAG,
   currentDateFormat,
@@ -16,6 +16,9 @@ import {
   defaultTime,
 } from "../../../js/common";
 
+import { uploadBytesResumable, ref, getDownloadURL } from "firebase/storage";
+import pathClose from "../../../icons/open.svg";
+
 export const ModalEdit = () => {
   const navigate = useNavigate();
 
@@ -23,13 +26,68 @@ export const ModalEdit = () => {
   const card = location.state.card;
   const flag = location.state.flag;
 
+  const promises = [];
+  const newFiles = [];
+
+  for (let i = 0; i < card.files?.length; i++) {
+    card.files[i].checked = true;
+  }
+
   const updateCard = async (values) => {
+    values.files = [];
+
+    for (let i = 0; i < card.files?.length; i++) {
+      if (card.files[i].checked === true) values.files.push(card.files[i]);
+      delete values.files[i].checked;
+    }
+    console.log(values.files);
+    for (let i = 0; i < newFiles?.length; i++) {
+      values.files.push(newFiles[i]);
+    }
+
+    console.log(values.files);
+
     await updateDoc(doc(db, "todos", values.id), {
       id: values.id,
       name: values.name,
       description: values.description,
       deadline: values.deadline,
       dateCompletion: values.dateCompletion,
+      files: values.files,
+    });
+  };
+
+  const handleCheckbox = (index, file) => {
+    const item = document.querySelector(`#a${index}`);
+    item.checked = item.checked ? false : true;
+    file.checked = file.checked ? false : true;
+  };
+
+  const handleChange = (e) => {
+    for (let i = 0; i < e.target.files.length; i++) {
+      const newImage = e.target.files[i];
+      promises.push(uploadImageAsPromise(newImage));
+    }
+  };
+
+  const uploadImageAsPromise = (file) => {
+    return new Promise(function (resolve, reject) {
+      const storageRef = ref(storage, `${card.id}/${file.name}`);
+      const task = uploadBytesResumable(storageRef, file);
+
+      task.on(
+        "state_changed",
+        (snapshot) => {},
+        (err) => {},
+        () => {
+          getDownloadURL(task.snapshot.ref).then((downloadURL) => {
+            newFiles.push({
+              name: file.name,
+              url: downloadURL,
+            });
+          });
+        }
+      );
     });
   };
 
@@ -73,10 +131,38 @@ export const ModalEdit = () => {
                 name="deadline"
                 onChange={props.handleChange}
                 onBlur={props.handleBlur}
-                min={currentDateFormat}
                 value={props.values.deadline}
               />
             </label>
+            <div className="files">
+              {card.files?.map((file, index) => (
+                <div className="file">
+                  <label
+                    htmlFor={index}
+                    onClick={() => handleCheckbox(index, file)}>
+                    <input
+                      type="checkbox"
+                      id={"a" + index}
+                      onChange={props.handleChange}
+                      onBlur={props.handleBlur}
+                      name="checkbox"
+                      checked={file.checked ? true : false}
+                    />
+                    {file.name}
+                  </label>
+                  <a
+                    className="file__url"
+                    href={file.url}
+                    download
+                    key={file.url}
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    <img src={pathClose} alt="" />
+                  </a>
+                </div>
+              ))}
+            </div>
+            <input type="file" multiple onChange={handleChange} />
             <label className="toggle" htmlFor="toggle">
               <input
                 type="checkbox"
@@ -87,6 +173,7 @@ export const ModalEdit = () => {
                 onChange={props.handleChange}
                 onBlur={props.handleBlur}
               />
+
               <span className="toggle__track">
                 <span className="toggle__track_indicator"></span>
               </span>
